324a2a7bb766b5ea88128997058378b267174e40
Glindley23 <graham.lindley23@gmail.com>
2022-11-12 18:32:29 -0500

    modal progress

--- js/index.js
+++ js/index.js
@@ -405,3 +405,34 @@ scrollOffset = 0;
 
 init();
 animate();
+function openModal(modal) {
+    if (modal === null) return
+    modal.classList.add('active')
+    overlay.classList.add('active')
+}
+function closeModal(modal) {
+    if (modal === null) return
+    modal.classList.remove('active')
+    overlay.classList.remove('active')
+
+
+let openModalButtons = document.querySelectorAll('[data-modal-target]')
+let closeModalButtons = document.querySelectorAll('[data-close-button]')
+let overlay = document.getElementById('overlay');
+
+openModalButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        let modal = document.querySelector(button.dataset.modalTarget)
+        openModal(modal);
+    })
+})
+
+closeModalButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        let modal = button.closest('.modal')
+        closeModal(modal);
+    })
+})
+
+
+}
